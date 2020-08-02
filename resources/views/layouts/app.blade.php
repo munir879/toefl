34fcdcb9715e6adaff6a50248f24ee6f3cf44408
kurniagungk <37070156+kurniagungk@users.noreply.github.com>
2020-08-02 19:59:19 +0700

    fix login

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -46,11 +46,11 @@
                         <!-- Authentication Links -->
                         @guest
                         <li class="nav-item">
-                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            <a class="nav-link" href="{{ route('user.login') }}">{{ __('Login') }}</a>
                         </li>
                         @if (Route::has('register'))
                         <li class="nav-item">
-                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                            <a class="nav-link" href="{{ route('user.registration') }}">{{ __('Register') }}</a>
                         </li>
                         @endif
                         @else
